1867099dddbfc91df304ad9cd0c2faa57d6d40c9
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-03-17 11:28:15 -0300

    Delete exercicio-02.py

--- semana03/exercicio-02.py
+++
@@ -1,9 +0,0 @@
-nome = input("Qual é seu nome")
-
-n1 = float(input("Quap é a primeira nota?"))
-
-n2 = float(input("Qual o valor da segunda nota?"))
-
-m = (n1 + n2) /n2
-
-print("Sua nota final é", m)
