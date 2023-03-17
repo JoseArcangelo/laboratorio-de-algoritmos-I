7c3c190bb3ae73c5820c2f18ea05c6b62d80952a
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-03-17 11:32:12 -0300

    Create exercicio-01.py

---
+++ semana02/exercicio-01.py
@@ -0,0 +1,9 @@
+nome = input("Qual é seu nome?")
+
+n1 = float(input("Qual é sua primeira nota?"))
+
+n2 = float(input("Qual é sua segunda nota?"))
+
+m = (n1 + n2) / 2 
+
+print("A media final de", nome, "será", m)
