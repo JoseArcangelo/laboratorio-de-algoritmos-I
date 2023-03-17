afc358daf17aff5635eb0005a3aeae799a3bf3c7
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-03-17 11:37:32 -0300

    Create exercicio-02.py

---
+++ semana02/exercicio-02.py
@@ -0,0 +1,9 @@
+base = float(input("Informe a base do retangulo:"))
+
+altura = float(input("Informe a altura do retangulo:"))
+
+area = base * altura
+
+perimetro = (base * 2) + (altura * 2)
+
+print("A area desse retangulo é", area, "e seu perimetro é", perimetro)
