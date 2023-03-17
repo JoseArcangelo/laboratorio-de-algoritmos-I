6afe03cd0f455dcac21310c55fd1ac1218845cd3
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-03-17 13:44:32 -0300

    Create exercicio-03.py

---
+++ semana02/exercicio-03.py
@@ -0,0 +1,9 @@
+nome = "José Arcangelo"
+
+idade = 19
+
+sexo = "Masculino"
+
+curso ="Sistmas de Informação"
+
+print("Olá, meu nome é", nome, "sou do sexo", sexo, "tenho", idade, "anos e estudo", curso)
