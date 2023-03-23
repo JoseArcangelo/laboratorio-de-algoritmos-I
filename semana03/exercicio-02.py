424588e0a2540296b37f6b3a7a722c2a5c1f7ba6
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-03-23 20:45:59 -0300

    Create exercicio-02.py

---
+++ semana03/exercicio-02.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite o primeiro numero:"))
+
+n2 = int(input("Digite o segundo numero:"))
+
+if n1 < n2:
+    print("Valores na ordem crescente:", n1, n2)
+    
+else:
+    print("Valores em ordem cresente", n2, n1)
