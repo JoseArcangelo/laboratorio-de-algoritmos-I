daa81311815723744249a5fc388dfc350e0d3146
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-03-23 20:11:47 -0300

    Create exercicio-01.py

---
+++ semana03/exercicio-01.py
@@ -0,0 +1,10 @@
+horas = float(input("Quantidade de horas:"))
+
+salario = horas * 35
+
+if salario <=1000:
+    aumento = salario + 300
+    print("Salario final com aumento sera R$", aumento)
+    
+else:
+    print("Salario final sem aumento sera R$", salario)
