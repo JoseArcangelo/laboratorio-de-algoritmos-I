3e1050289df6e190da950ff515bd0c571b257489
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-03-23 21:23:03 -0300

    Create exercicio-04.py

---
+++ semana03/exercicio-04.py
@@ -0,0 +1,7 @@
+preco = float(input("Digite o preco atual da gasolina:"))
+
+valor = float(input("Digite o valor do pagante:"))
+
+litros = valor / preco
+
+print("O cliente conseguiu colocar no tanque de seu automovel", litros, "l de combustivel")
