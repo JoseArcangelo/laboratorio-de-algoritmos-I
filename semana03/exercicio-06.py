5809bb75e2ab7f33bf69f5a2b726ca6fefddb736
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-03-23 21:00:45 -0300

    Create exercicio-06.py

---
+++ semana03/exercicio-06.py
@@ -0,0 +1,8 @@
+numero = float(input("Digite um numero:"))
+
+if numero >= 1:
+    numeroFinal = numero * 2
+    print("O dobro desse valor sera:", numeroFinal)
+    
+else:
+    print("VALOR INVALDO!")
