1dc9ae6e38ed39e504bc0023e393661724029438
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-03-31 16:17:08 -0300

    Create exercicio-04.py

---
+++ semana-4/exercicio-04.py
@@ -0,0 +1,30 @@
+n1 = float(input("Informe a primeira nota do aluno:"))
+
+n2 = float(input("Informe a segunda nota do aluno:"))
+
+m = (n1 + n2) / 2
+
+if m >=9 and m <=10:
+    print("A primeira nota do aluno é", n1, ",a segunda é", n2, " e sua media é", m)
+    print("O conceito do aluno será = A")
+    print("O aluno está APROVADO!")
+    
+elif m >= 7.5 and m < 9:
+    print("A primeira nota do aluno é", n1, ",a segunda é", n2, " e sua media é", m)
+    print("Com isso o conceito do aluno será = B")
+    print("O aluno está APROVADO!")
+    
+elif m >= 6 and m < 7.5:
+    print("A primeira nota do aluno é", n1, ",a segunda é", n2, " e sua media é", m)
+    print("Com isso o conceito do aluno será = C")
+    print("O aluno está APROVADO!")
+    
+elif m >= 4 and m < 6:
+    print("A primeira nota do aluno é", n1, ",a segunda é", n2, " e sua media é", m)
+    print("Com isso o conceito do aluno será = D")
+    print("O aluno está REROVADO!")
+    
+else:
+    print("A primeira nota do aluno é", n1, ",a segunda é", n2, " e sua media é", m)
+    print("Com isso o conceito do aluno será = E")
+    print("O aluno está REPROVADO!")
