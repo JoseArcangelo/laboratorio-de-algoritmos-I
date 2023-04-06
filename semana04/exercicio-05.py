6f5df510234ef503c7825e82b9f20aea739b3e25
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-04-06 14:39:20 -0300

    Delete semana04 directory

--- semana04/exercicio-05.py
+++
@@ -1,9 +0,0 @@
-veiculo = str(input("Qual veiluco voce pode dirigir?"))
-
-tipoCarteira = str(input("Qual o tipo de sua carteira de motorista?(A,B,C,D ou E)")).upper()
-
-if tipoCarteira == "A":
-    print("Voce pode dirigir motos e triciclos")
-    
-elif tipoCarteira == "B":
-    print("Voce pode dirigir")
