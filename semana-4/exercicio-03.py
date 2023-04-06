5307f697c44021405d1e9ef65d141f2f42660673
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-04-06 15:23:42 -0300

    Update exercicio-03.py

--- semana-4/exercicio-03.py
+++ semana-4/exercicio-03.py
@@ -1,18 +1,18 @@
-n1 = float(input("Informe a primeira nota:"))
+n1 = float(input("Informe a primeira nota do aluno:"))
 
-n2 = float(input("Informe a segunda nota:"))
+n2 = float(input("Informe a segunda nota do aluno:"))
 
 media = (n1 + n2) / 2
 
-frequencia = float(input("Informe seu percentual(%) de frequencia :"))
+frequencia = float(input("Informe o percentual(%) de frequencia do aluno:"))
 
 
 
 if media >= 7 and frequencia >= 75:
-    print("O aluno esta aprovado!")
+    print("O aluno esta APROVADO!, com media", media, "e frequenica de", frequencia, "%")
     
 elif media >=4 and media < 7 and frequencia > 75:
-    print("O aluno ficou de exame!")
+    print("O aluno ficou de exame!, com media", media, "e frequencia de", frequencia, "%")
     
 else:
-    print("Reprovado!")
+    print("O aluno esta REPROVADO!, com media", media, "e frequencia de", frequencia, "%")
