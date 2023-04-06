8f042acb2ca876cb1131565d545341e22ccd47c9
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-04-06 10:50:45 -0300

    Create exercicio-06.py

---
+++ semana-4/exercicio-06.py
@@ -0,0 +1,31 @@
+quantidadeMaca = int(input("Quantos quilos de maça voce quer comprar?"))
+
+quantidadeMorango = int(input("Quantos quilos de morango voce quer comprar?"))
+
+quantidadeTotal = quantidadeMaca + quantidadeMorango
+
+if quantidadeTotal <= 5:
+    valorMaca = quantidadeMaca * 1.8
+    
+    valorMorango = quantidadeMorango * 2.5
+    
+    valorTotal = valorMorango + valorMaca
+    
+    print("Você adquiriu", quantidadeMaca, "Kg de maças e", quantidadeMorango,
+    "Kg de morangos, o valor total da compra foi de R$", valorTotal)
+    
+elif quantidadeTotal > 5:
+    valorMaca = quantidadeMaca * 1.5
+    
+    valorMorango = quantidadeMorango * 2.2
+    
+    valorTotal = valorMaca + valorMorango
+    
+    if quantidadeTotal >= 8 or valorTotal > 25:
+        valorTotal = valorTotal * 0.9
+        
+print("Foi adquirido", quantidadeMaca, "Kg de maças e", quantidadeMorango,
+    "Kg de morangos, o valor total da compra foi de R$", valorTotal)
+        
+        
+    
