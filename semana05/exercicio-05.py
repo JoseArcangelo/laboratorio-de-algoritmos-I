2dc9fbda4487896c76f5ed052bd11638eceb0b2e
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-04-08 11:15:30 -0300

    Update exercicio-05.py

--- semana05/exercicio-05.py
+++ semana05/exercicio-05.py
@@ -1,16 +1,14 @@
 totalPessoas = 0
-salario = 0
 mediaSalario = 0
 mulhereSalario100 = 0
-sexo = " "
 maiorIdade = 0
 menorIdade = 0
-idade = 0
+
 while totalPessoas < 10:
     totalPessoas += 1
     salario = float(input("Informe o SALARIO da nova pessoa participante da pesquisa:"))
     mediaSalario =mediaSalario + salario 
-    sexo = str(input("Informe o SEXO da nova pessoa participante da pesquisa(Masculino/Feminino:)")).upper()
+    sexo = str(input("Informe o SEXO da nova pessoa participante da pesquisa(Masculino/Feminino):")).upper()
     
     if sexo == "FEMININO" and salario <= 100:
         mulhereSalario100 += 1
@@ -35,6 +33,6 @@ while totalPessoas < 10:
 mediaSalario = mediaSalario / totalPessoas
       
 print("A media de salario desse grupo de 10 pessoas é", mediaSalario)
-print("A maior idade desse grupo é", maiorIdade, "a menor idade é", menorIdade)
+print("A maior idade desse grupo é", maiorIdade, "e a menor idade é", menorIdade)
 print("A quantidade de mulheres com sálario de ate R$100,00 é:", mulhereSalario100)
 
