6db254d2e21e9d470ec674bc49808d3666ab3516
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-04-13 10:33:20 -0300

    Update exercicio-01.py

--- semana05/exercicio-01.py
+++ semana05/exercicio-01.py
@@ -1,31 +1,33 @@
 opcao = 0
-caixa = float(input("Valor em caixa:"))
-while opcao != 4:
-    print("1- Realizar venda")
-    print("2- Retirar dinheiro")
-    print("3- Dinheiro em caixa")
-    print("4- Sair")
-    opcao = int(input("Opcao:"))
+idade = 0
+altura = 0
+totalPessoas = 0
+
+while opcao != 3:
+    print("1- Cadastrar pessoa")
+    print("2-Mostrar media parcial de altura e idade")
+    print("3- Sair")
+    opcao = int(input("Escolha uma opção:"))
     
     if opcao == 1:
-        venda = float(input("Valor da venda:"))
-        caixa += venda
+        totalPessoas += 1
+
+        idade += int(input("Informe a sua idade:"))
+        mediaIdade = idade / totalPessoas
+        
+        altura += float(input("Informe a sua altura:"))
+        mediaAltura = altura / totalPessoas
+        
         
     elif opcao == 2:
-        retirada = float(input("Valor da retirada:"))
+        print("A media parcial de idade é", mediaIdade, "e a de altura é", mediaAltura)
         
-        if caixa >= retirada:
-            caixa = caixa - retirada
         
-        else:
-            print("Valor insuficiente!")
-            
     elif opcao == 3:
-        print("Valor em caixa:", caixa)
-        
-    elif opcao == 4:
-        print("Volte sempre na loja do 1000tinho!")
+        print("A media de idade final e oficial é", mediaIdade, "e a de alutra é", mediaAltura)
+        print("Saindo...")
         
     else:
-        print("Opcão inválida")
+        print("Opção inválida!")
     
+        
