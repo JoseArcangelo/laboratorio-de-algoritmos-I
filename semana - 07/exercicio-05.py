45f8a43adeaed7af0acdb8b77f4a1a16fa5335c2
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-04-20 22:06:57 -0300

    Update exercicio-05.py

--- semana - 07/exercicio-05.py
+++ semana - 07/exercicio-05.py
@@ -4,8 +4,12 @@ olhosAzuis = 0
 olhosVerdes = 0
 olhosCastanhos = 0
 cabeloLoiro = 0
+cabeloCastanho = 0
+cabeloPreto = 0
+maiorIdade = 0
+fivl = 0
 
-for i in range(0, 1):
+for i in range(1, 3):
     certo = 0
 
     while certo == 0:
@@ -40,11 +44,40 @@ for i in range(0, 1):
             print("Opcao invalida! informe somente uma das opcoes da pergunta")
             
     while certo == 2:
-        corCabelos = str(input("Informe a cor de seu cabelo: L (loiros), C (castanhos) e P (pretos)"))
+        corCabelos = str(input("Informe a cor de seu cabelo: L (loiros), C (castanhos) e P (pretos)")).upper()
         
         if corCabelos == "L":
             cabeloLoiro += 1
-            certo += 1
+            certo = 3
+            
+        elif corCabelos == "C":
+            cabeloCastanho +=1
+            certo = 3
+            
+        elif corCabelos == "P":
+            cabeloPreto += 1
+            certo = 3
+            
+        else:
+            print("Opcao invalida! informe somente uma das opcoes da pergunta")
+            
+    idade = int(input("Informe a sua idade:"))
+    
+    if i == 1:
+        maiorIdade = idade
+
+    if idade > maiorIdade:
+        maiorIdade = idade
+        
+    if sexo == "F" and idade > 18 and idade < 35 and corOlhos == "V" and corCabelos == "L":
+        fivl += 1
+        print(fivl)
+
+      
+        
+    
+    
+        
             
         
         
