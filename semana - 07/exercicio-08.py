a368a998de1d3919bfdffb3167328c0c74faae5c
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-04-27 14:20:02 -0300

    Update exercicio-08.py

--- semana - 07/exercicio-08.py
+++ semana - 07/exercicio-08.py
@@ -1,7 +1,7 @@
 fora = 0
 dentro = 0
 
-for i in range(1, 11):
+for i in range(0, 10):
     i = int(input("Informe um numero:"))
     if i > 10 and i < 20:
         dentro += 1
@@ -9,5 +9,4 @@ for i in range(1, 11):
     else:
         fora += 1
         
-print("Dentre esses", (fora + dentro), "numeros", dentro, "estão entre 10 e 20 e", fora, "estao fora")
-        
+print("Dentre esses numeros informados", dentro, "estão entre 10 e 20, e", fora, "estao fora")
