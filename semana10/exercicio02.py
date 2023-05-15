7b37b6248985dafff3324f6f327585e10934f277
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-05-15 14:05:53 -0300

    Create exercicio02.py

---
+++ semana10/exercicio02.py
@@ -0,0 +1,17 @@
+def lerNumero():
+    numero = int(input("Informe um numero:"))
+    return numero
+    
+def verificar(numero):
+    if numero % 2 == 1 or numero == 2:
+        print("True")
+        
+    else:
+        print("False")
+ 
+
+def main():
+    numero = lerNumero()
+    primo = verificar(numero)
+    
+main()
