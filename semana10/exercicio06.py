1246c50511f34df4f4c4c7a32b38d4e5a6981d7f
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-05-15 14:57:26 -0300

    Update exercicio06.py

--- semana10/exercicio06.py
+++ semana10/exercicio06.py
@@ -1,1 +1,23 @@
+def pesquisa(sim, nao):
+    for i in range(5):
+        teste = 0
+        while teste != 1:
+            pergunta = str(input("Resposta da pesquisa(Sim/Nao)")).upper()
+            if pergunta == "SIM":
+                sim += 1
+                teste = 1 
+            elif pergunta == "NAO":
+                nao += 1
+                teste = 1
+            else:
+                print("Resposta invalida!!! responda somente com Sim ou Nao!")
+    return sim, nao
 
+def main():
+    sim = 0
+    nao = 0
+    sim, nao = pesquisa(sim, nao)
+    print("O total de pessoas que responderam sim foi:", sim)
+    print("O total de pessoas que responderam Não foi", nao)
+    
+main()
