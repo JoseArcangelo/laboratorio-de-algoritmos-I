37206b937ee65539a44b56122a2df883f65ef33f
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-05-18 13:48:27 -0300

    Update exercicio06.py

--- semana10/exercicio06.py
+++ semana10/exercicio06.py
@@ -2,11 +2,11 @@ def pesquisa(sim, nao):
     for i in range(5):
         teste = 0
         while teste != 1:
-            pergunta = str(input("Resposta da pesquisa(Sim/Nao)")).upper()
+            pergunta = str(input("Resposta da pesquisa(Sim/Nao):")).upper()
             if pergunta == "SIM":
                 sim += 1
                 teste = 1 
-            elif pergunta == "NAO":
+            elif pergunta == "NAO" or pergunta == "NÃO":
                 nao += 1
                 teste = 1
             else:
@@ -17,7 +17,7 @@ def main():
     sim = 0
     nao = 0
     sim, nao = pesquisa(sim, nao)
-    print("O total de pessoas que responderam sim foi:", sim)
+    print("O total de pessoas que responderam Sim foi:", sim)
     print("O total de pessoas que responderam Não foi", nao)
     
 main()
