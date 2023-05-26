eaf9357ce9b2f5db2dae884977f2f038cc1fcf38
JoseArcangelo <127755871+JoseArcangelo@users.noreply.github.com>
2023-05-25 21:26:42 -0300

    Create exercicio03.py

---
+++ semana12/exercicio03.py
@@ -0,0 +1,32 @@
+def lerNumeros(lista):
+    for i in range(5):
+        n = float(input("Digite um valor:"))
+        lista.append(n)
+    return lista
+        
+def maiorMenorNumero(lista):
+    maiorNumero = lista[0]
+    menorNumero = lista[0]
+    posicaoMaior = 0
+    posicaoMenor = 0
+    contador = 0
+    for i in lista:
+        if i > maiorNumero:
+            maiorNumero = i
+            posicaoMaior = contador
+            
+        elif i < menorNumero:
+            menorNumero = i
+            posicaoMenor = contador
+            
+        contador += 1
+        
+    print("Maior numero da lista:", maiorNumero, ",e sua posicao:", posicaoMaior)
+    print("Maior numero da lista:", menorNumero, ",e sua posicao:", posicaoMenor)    
+
+def main():
+    lista = []
+    lerNumeros(lista)
+    maiorMenorNumero(lista)
+    
+main()
